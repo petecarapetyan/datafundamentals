1f20b4e157f610a82dcbdfa049b62f1e85077336
Pete Carapetyan <pete.carapetyan@gmail.com>
2025-04-04 18:19:10 -0500

    deleted first batch of njk files no longer in use

--- brandMigration.sh
+++ brandMigration.sh
@@ -1,17 +1,30 @@
-make this runnable, remove the numbers, check out a previous that has old_docs, etc
+# make this runnable, remove the numbers, check out a previous that has old_docs, etc
 
- 1127  rm site/nulla-nisi
- 1128  rm -rf site/nulla-nisi
- 1129  rm -rf site/occaecat
- 1130  rm -rf site/pariatur
- 1131  rm -rf site/index.md
- 1132  rm rf site/sitemap.njk
- 1133  mv old_docs/content site
- 1134  mv old_docs/packages site
- 1135  mv old_docs/pre2015 site
- 1136  mv old_docs/index.md site
- 1137  mv mv old_docs/about site
- 1138  mv old_docs/sitemap.njk site
- rm static logo.svg
- remove images using separate python script
- rm site/_includes/_joiningBlocks/_layoutHome/
+#  1127  rm site/nulla-nisi
+#  1128  rm -rf site/nulla-nisi
+#  1129  rm -rf site/occaecat
+#  1130  rm -rf site/pariatur
+#  1131  rm -rf site/index.md
+#  1132  rm rf site/sitemap.njk
+#  1133  mv old_docs/content site
+#  1134  mv old_docs/packages site
+#  1135  mv old_docs/pre2015 site
+#  1136  mv old_docs/index.md site
+#  1137  mv mv old_docs/about site
+#  1138  mv old_docs/sitemap.njk site
+#  rm static logo.svg
+#  remove images using separate python script
+rm -rf site/_includes/_joiningBlocks/_layoutHome/ 
+rm -rf site/_includes/_joiningBlocks/content
+rm -rf site/_includes/_joiningBlocks/_layoutIndex/content
+rm -rf site/_includes/_joiningBlocks/_layoutIndex/content
+rm -rf site/_includes/_joiningBlocks/_layoutSidebar/
+rm -rf site/_includes/_joiningBlocks/bottom         
+rm -rf site/_includes/_joiningBlocks/footer 
+rm -rf site/_includes/_joiningBlocks/head
+rm -rf site/_includes/_joiningBlocks/header 
+rm -rf site/_includes/partials             
+rm site/_includes/snippet/header-dark-switch.njk
+rm site/_includes/snippet/header-logo.njk
+
+
