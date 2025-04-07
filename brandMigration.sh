9e62d830c83c90b37f446a5fa8894eb05f4fa6b7
Pete Carapetyan <pete.carapetyan@gmail.com>
2025-04-07 12:08:16 -0500

    massive cleanup of old directories gawd help me

--- brandMigration.sh
+++ brandMigration.sh
@@ -1,19 +1,21 @@
 # make this runnable, remove the numbers, check out a previous that has old_docs, etc
 # you have to start with a migration2025 branch to have the old_docs etc to migrate froms
-#  1127  rm site/nulla-nisi
-#  1128  rm -rf site/nulla-nisi
-#  1129  rm -rf site/occaecat
-#  1130  rm -rf site/pariatur
-#  1131  rm -rf site/index.md
-#  1132  rm rf site/sitemap.njk
-#  1133  mv old_docs/content site
-#  1134  mv old_docs/packages site
-#  1135  mv old_docs/pre2015 site
-#  1136  mv old_docs/index.md site
-#  1137  mv mv old_docs/about site
-#  1138  mv old_docs/sitemap.njk site
-#  rm static logo.svg
-#  remove images using separate python script
+# - if you get lost and need old files back, checkout the migration2025 branch
+
+rm -rf site/nulla-nisi
+rm -rf site/occaecat
+rm -rf site/pariatur
+rm -rf site/index.md
+rm rf site/sitemap.njk
+mv old_docs/content site
+mv old_docs/packages site
+mv old_docs/pre2015 site
+mv old_docs/index.md site
+mv mv old_docs/about site
+mv old_docs/sitemap.njk site
+rm static/logo.svg
+rm -rf extra
+#  remove images using separate python script /Users/petecarapetyan/work/appwriter2035/sites/ImageCleanupUtility.py
 rm -rf site/_includes/_joiningBlocks/_layoutHome/ 
 rm -rf site/_includes/_joiningBlocks/content
 rm -rf site/_includes/_joiningBlocks/_layoutIndex/content
@@ -27,7 +29,6 @@ rm -rf site/_includes/partials
 rm site/_includes/snippet/header-dark-switch.njk
 rm site/_includes/snippet/header-logo.njk
 rm static/images/bulksplash-*.jpg
-rm static/logo.svg
 rm site/_includes/layout-simulator.njk
 rm site/_includes/layout-home-background.njk
 
